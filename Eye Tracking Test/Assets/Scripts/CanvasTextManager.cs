951bfbbd6165ef5d0c7b696827c191bd100cd794
Douglas Titze <douglasmarcost2@gmail.com>
2024-11-15 17:02:22 -0500

    Add tutorial canvas and an eye ray toggle canvas

---
+++ Eye Tracking Test/Assets/Scripts/CanvasTextManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[System.Serializable] public class Sections
+{
+    public string Header;
+
+    [TextArea(5, 10)]
+    public string Text;
+}
+
+public class CanvasTextManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI Header;
+    [SerializeField] private TextMeshProUGUI Text;
+    [SerializeField] private Button btn_back;
+    [SerializeField] private Button btn_next;
+    [SerializeField] private GameObject cubes;
+    [Space]
+    [SerializeField] private List<Sections> TextSections = new List<Sections>();
+    
+    [HideInInspector] public int curSection = 0;
+    private int numberOfSections;
+    private void Start()
+    {
+        numberOfSections = TextSections.Count;
+        changeSection(curSection);
+    }
+
+    public void changeSection(int sectionNum)
+    {
+        if (sectionNum < numberOfSections)
+        {
+            // Update canvas text and curSection
+            curSection = sectionNum;
+            Header.text = TextSections[curSection].Header;
+            Text.text = TextSections[curSection].Text;
+
+            // Update buttons to avoid going out of bounds
+            updateButtons();
+
+            // Check if the cubes section is being displayed
+            toggleCubes();
+
+        }
+    }
+
+    private void updateButtons()
+    {
+        if (curSection == numberOfSections - 1)
+        {
+            btn_next.interactable = false;
+            btn_back.interactable = true;
+        }
+        else if (curSection == 0)
+        {
+            btn_back.interactable = false;
+            btn_next.interactable = true;
+        }
+        else
+        {
+            btn_back.interactable = true;
+            btn_next.interactable = true;
+        }
+    }
+
+    private void toggleCubes()
+    {
+        if (curSection == 2)
+        {
+            cubes.SetActive(true);
+        }
+        else
+        {
+            cubes.SetActive(false);
+        }
+    }
+}
